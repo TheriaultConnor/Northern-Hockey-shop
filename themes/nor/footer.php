0d9e093072c195093915779e3987083f0013bf2c
Connor Theriault <connor.theriault@dcmail.ca>
2022-02-14 18:37:49 -0500

    footer.php finished

--- themes/nor/footer.php
+++ themes/nor/footer.php
@@ -12,8 +12,8 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nor' ) ); ?>">
+		<div class="grid-x site-info">
+			<div class="cell large 4"><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nor' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'nor' ), 'WordPress' );
@@ -23,7 +23,13 @@
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nor' ), 'nor', '<a href="http://connortheriault.ca/">Connor Theriault</a>' );
-				?>
+				?></div>
+				<div class="large-8"></div>
+				<div class=" large-12"><ul>
+					<li>Contact Us</li>
+					<li>About Us</li>
+					<li>Jobs</li>
+				</ul></div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
