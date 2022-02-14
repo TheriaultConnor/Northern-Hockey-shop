5571d1b0a13134c0c12243b715e9ceba4d064c2a
Connor Theriault <connor.theriault@dcmail.ca>
2022-02-13 22:26:59 -0500

    Assets folder created and js file moved to assets

--- themes/nor/inc/customizer.php
+++ themes/nor/inc/customizer.php
@@ -55,6 +55,6 @@ function nor_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function nor_customize_preview_js() {
-	wp_enqueue_script( 'nor-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'nor-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'nor_customize_preview_js' );
