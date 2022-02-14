7119fa4d0be9be538f4e863e6a90a63a90f8f3ab
Connor Theriault <connor.theriault@dcmail.ca>
2022-02-14 15:48:36 -0500

    start files for header.php

--- themes/nor/header.php
+++ themes/nor/header.php
@@ -20,12 +20,28 @@
 	<?php wp_head(); ?>
 </head>
 
+<header class="grid-x header-main">
+<div class="large-5"><nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nor' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?></div>	
+<div class="cell large-3"><h1 id="site-logo">Northern Hockey</h1></div>
+<div class="cell large-4"><?php get_search_form(); ?></div>
+
+</header>
+
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'nor' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<header id="masthead" class="site-header" style="display:none">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
@@ -38,7 +54,7 @@
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
-			$nor_description = get_bloginfo( 'description', 'display' );
+			$nor_description = get_bloginfo(  );
 			if ( $nor_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $nor_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
