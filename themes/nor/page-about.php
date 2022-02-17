7052c47c2e59be6bcfd9ae8793d27a344478199a
Connor Theriault <connor.theriault@dcmail.ca>
2022-02-17 14:58:33 -0500

    Completed about page

---
+++ themes/nor/page-about.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Northern_Hockey
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+	
+	<div class="grid-container">
+    <div class="grid-x grid-margin-x">
+	<div class="cell large-4"></div>
+    <div class="cell large-4"><h1 class="h1-about" >About Us</h1></div>
+	<div class="cell large-4"></div>
+	<div class="cell large-12"><hr></div>
+
+	<div class="cell large-6"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla volutpat aliquet. Donec suscipit metus eget nunc aliquet pharetra. Nunc ut mi gravida, fringilla tellus et, pulvinar orci. Integer consequat risus nisi, quis ornare sapien ultrices vel. Pellentesque porttitor justo pretium tincidunt tempor. Aenean id sodales risus. Integer sollicitudin mauris dolor.
+
+Sed mattis dolor ut ipsum lacinia convallis. Suspendisse bibendum iaculis velit sit amet congue. Curabitur sagittis sem massa, sit amet facilisis nulla viverra vel. Fusce ut nulla in velit dapibus dictum. Nullam porta ante sit amet ipsum auctor accumsan. Curabitur quis rutrum odio. Proin ac justo in justo sodales dignissim vitae a lacus. Phasellus sit amet lacus in libero ullamcorper luctus ornare sed dui. Nam consectetur pulvinar nibh vitae ultricies. Quisque rutrum a enim et scelerisque. Suspendisse potenti. Nulla facilisi. Duis nisl leo, laoreet </p></div>
+
+	<div class="cell large-6"><img src="http://theriault100976473.local/wp-content/uploads/2022/02/Image-2.png" alt=""></div>
+
+  </div>
+</div>
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
