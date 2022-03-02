628a17ee342b76e095fe1649c5b3f87c941af094
Connor Theriault <connor.theriault@dcmail.ca>
2022-03-02 16:24:21 -0500

    Added woocommerce

--- themes/nor/single.php
+++ themes/nor/single.php
@@ -9,31 +9,40 @@
 
 get_header(); ?>
 
-<main id="primary" class="site-main">
 
+<div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
   
-  <?php
-		while ( have_posts() ) :
-			the_post();
+		<?php echo get_the_content();  ?>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+  
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */
+            get_template_part( 'content', get_post_format() );
+  
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+  
+            // Previous/next post navigation.
+           
+  
+        // End the loop.
+        endwhile;
+        ?>
+  
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'nor' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'nor' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+<?php
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-</main><!-- #main -->
-
-<?php get_footer(); ?>
+get_footer();
 
 
