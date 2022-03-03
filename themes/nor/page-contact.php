926e2b4ead448210d28be7b32e60f3a89d22f159
Connor Theriault <connor.theriault@dcmail.ca>
2022-03-03 16:52:49 -0500

    Removing old form

--- themes/nor/page-contact.php
+++ themes/nor/page-contact.php
@@ -17,37 +17,7 @@ get_header();
 
 	<main id="primary" class="site-main">
 	
-<div class="grid-container">
-<div class="grid-x grid-margin-x">
 
-<div class="cell large-4"></div>
-    <div class="cell large-4"><h1 class="h1-about" >Contact Us</h1></div>
-	<div class="cell large-4"></div>
-	<div class="cell large-12"><hr></div>
-
-<div class="form-container">
-<form>
-
-<label for="fname">First Name</label>
-<input type="text" id="fname" name="firstname" placeholder="Your name..">
-
-<label for="lname">Last Name</label>
-<input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-<label for="lname">Email</label>
-<input type="text" id="Email" name="lastname" placeholder="Your Email..">
-
-<label for="subject">Subject</label>
-<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-<input type="submit" value="Submit">
-
-</form>
-</div>
-
-	
-</div>
-</div>
 	</main><!-- #main -->
 
 <?php
